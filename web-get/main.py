660851a80d20a766f23280b65bfb148ad6dee185
stephanos zunez <stephanos.zunez@gmail.com>
2021-08-18 13:44:06 -0500

    dev merged to main

--- web-get/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-import os
-import urllib.request
-
-#webUrl = urllib.request.urlopen('https://www.hcdistrictclerk.com/eDocs/Public/Search.aspx')
-webUrl = urllib.request.urlopen('https://www.youtube.com/user/guru99com')
-print("reqsut code: " + str(webUrl.getcode()))
