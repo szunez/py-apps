0cda38b48507bd1bb2f85218eab0d14ceee0f2bf
Stephanos Zunez <stephanos.zunez@gmail.com>
2023-09-11 22:32:41 -0500

    textbox with stats added to graph

--- wordcount.py
+++ wordcount.py
@@ -1,8 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import statistics
+from scipy.stats import norm
+import scipy.stats as stats
 from scipy.optimize import minimize
 from scipy.stats import gamma
+def neg_log_likelihood(params, word_count):
+    k, theta = params
+    if k <= 0 or theta <= 0:
+        return np.inf  # Ensure positive parameters
+    return -np.sum(gamma.logpdf(word_count, k, loc=0, scale=theta))
 input_text = input("Give me some words and I will count them for you:\n")
 words = input_text.split()
 word_count = []
@@ -12,26 +18,20 @@ print(f"Total characters in the text: {len(input_text)}")
 print(f"Total words in the text: {word_count}")
 print(f"Length of word {len(word_count)}")
 if len(word_count) > 1:
-    stdev = statistics.stdev(word_count)
-    mean = statistics.mean(word_count)
-    normal = statistics.NormalDist(mean,stdev)
+    stdev = np.std(word_count)
+    mean = np.mean(word_count)
+    normal = norm(loc=mean, scale=stdev)
     print(f"Average of word lengths: {mean:.2f}")
     print(f"Variance of word lengths: {stdev:.2f}")
 else:
     print("Standard Deviation cannot be calculated with fewer than 2 words.")
-def neg_log_likelihood(params, word_count):
-    k, theta = params
-    if k <= 0 or theta <= 0:
-        return np.inf  # Ensure positive parameters
-    return -np.sum(gamma.logpdf(word_count, k, loc=0, scale=theta))
 initial_guess = [2, 1] # Initial guess for parameters (you can adjust this)
 result = minimize(neg_log_likelihood, initial_guess, args=(word_count,), method='L-BFGS-B')
 k_estimated, theta_estimated = result.x
 x = np.linspace(0, 20, 1000)
-#pdf_values = [normal.pdf(val) for val in x]
 pdf_values = gamma.pdf(x, k_estimated, loc=0, scale=theta_estimated)
+textbox = f"Mean: {mean:.2f}\nStd Dev: {stdev:.2f}\nK: {k_estimated:.2f}\nTheta: {theta_estimated:.2f}"
 plt.figure(figsize=(8, 6))
-#plt.plot(x, pdf_values, label='PDF')
 plt.plot(x, pdf_values, 'r-', lw=2, label='Gamma PDF')
 plt.title('Distribution of word length')
 plt.xlabel('Word length')
@@ -39,4 +39,5 @@ plt.ylabel('Probability Density')
 plt.xlim(0,20)
 plt.grid(True)
 plt.legend()
+plt.text(16, 0.15, textbox, bbox=dict(facecolor='white', alpha=0.5))
 plt.show()
